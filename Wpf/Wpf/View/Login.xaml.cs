791e46d4b6e8268a73f6123624ff1f49b8129bc9
HanOy <339340195@qq.com>
2015-03-26 16:52:41 +0800

    first commit

---
+++ Wpf/Wpf/View/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf.View
+{
+    /// <summary>
+    /// Login.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void cancel_click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	Application.Current.Shutdown();
+            base.OnClosed(e);
+        }
+    }
+}
